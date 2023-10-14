9030bcfd1afdde50bebcea77aa778b726c4efe2c
Raj Kasaudhan <rajksd9@gmail.com>
2023-10-14 18:51:58 +0530

    homepageSection

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,10 +1,19 @@
+import CategoriesList from "../components/CategoriesList.jsx";
+import CustomerReview from "../components/CustomerReview.jsx";
 import Footer from "../components/Footer.jsx";
 import Header from "../components/Header.jsx";
+import SingleServiceContainer from "../components/SingleServiceContainer.jsx";
 
 export default function Home() {
   return (
-    <div>
+    <div className="overflow-hidden">
       <Header />
+      <CategoriesList name={"Home Repair Services"} />
+      <CategoriesList name={"Cleaning & Pest Control"} />
+      <SingleServiceContainer />
+      <CategoriesList name={"AC & Appliances"} />
+      <CustomerReview />
+
       <Footer />
     </div>
   );
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import insta from "../constants/images/insta.jpeg";
 import facebook from "../constants/images/facebook.png";
